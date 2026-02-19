d81c04343d546bc891c78a9d71d51cfa462259a2
Tempest <tempest@debian.tt>
2026-02-19 17:05:50 +0530

    Version 1

---
+++ main.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import cv2
+import numpy as np
+from tensorflow.keras.applications.mobilenet_v2 import (  # pyright: ignore[reportMissingImports]
+    MobileNetV2,
+    preprocess_input,
+    decode_predictions
+)
+from PIL import Image
+
+def load_model() :
+    model = MobileNetV2(weights="imagenet")
+    return model
+
+def preprocess_image(image) :
+    img = np.array(image)
+    img = cv2.resize(img, (224, 224))
+    img = preprocess_input(img)
+    img = np.expand_dims(img, axis=0)
+
+    return img
+
+def classify_image(model, image) :
+    try :
+        processed_image = preprocess_image(image)
+        predictions = model.predict(processed_image)
+        decoded_predictions = decode_predictions(predictions, top=3)[0]
+
+        return decoded_predictions
+    except Exception as e :
+        st.error(f"Error classifying image: {str(e)}")
+
+        return None
+
+def main() :
+    st.set_page_config("AI Image Classifier", page_icon="🤖", layout="centered")
+
+    st.title("AI Image Classifer")
+    st.write("Upload the image and let the AI tell what it is!")
+    
+    @st.cache_resource
+    def load_cached_model() :
+        return load_model()
+
+    model = load_cached_model()
+
+    uploaded_file = st.file_uploader("Choose and image...", type=["jpg", "png"])
+
+    if uploaded_file is not None :
+        image = st.image(uploaded_file, caption="Uploaded Image", use_container_width=True)
+        btn = st.button("Classify Image")
+
+        if btn :
+            with st.spinner("Analyzing Image...") :
+                image = Image.open(uploaded_file)
+                predictions = classify_image(model, image)
+
+                if predictions :
+                    st.subheader("Predictions")
+                    for _, label, score in predictions :
+                        st.write(f"**{label}**: {score:.2%}")
+
+if __name__ == "__main__" :
+    main()
